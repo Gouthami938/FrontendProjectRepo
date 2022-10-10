ee38d8969e2ed303667da594276fc908ea386778
Gouthami938 <70152870+Gouthami938@users.noreply.github.com>
2022-10-10 12:29:16 +0530

    platinum page

---
+++ Platinum.js
@@ -0,0 +1,63 @@
+import BaseUD from "../components/BaseUD";
+import {Card, CardHeader, Col, Container, Form, FormGroup, Input, Row,Button, CardBody} from "reactstrap";
+import { useState } from "react";
+import {toast } from 'react-toastify';
+import { loginUser } from "../services/user-service";
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+ import card from "./card.jpg";
+import BaseMem from "../components/BaseMem";
+
+function Platinum() {
+
+        const navigate=useNavigate();
+        const user= JSON.parse(localStorage.getItem('user-info'))
+        console.warn(user)
+        
+        function redirectp()
+        {
+          
+          navigate('/Acceptlist')
+          toast.success('Paltinum Membership Availed')
+        }
+
+
+    return (
+<div style={{
+ backgroundImage:'url("https://cdn.anewmode.com/wp-content/uploads/2018/07/how-to-make-him-fall-for-you-4.jpg")',width:1300,height:608,backgroundSize:'Cover'
+}}>
+<BaseMem>
+<Container>
+        
+        <Row className="mt-4">
+        
+                {/* {JSON.stringify(data)} */}
+        
+        
+                <Col  sm={4}>
+                <Card color="secondary" >
+                <CardHeader>
+        <h3><span style={{fontFamily:'Times new Roman',fontWeight:'bold'}}>Platinum Subscription</span></h3>
+       <h5><span style={{fontFamily:'Times new Roman',fontWeight:'bold'}}>From INR</span></h5>  
+        <h5><span style={{fontFamily:'Times new Roman',fontWeight:'bold'}}>Local charges may apply</span></h5>
+        <h3><span style={{fontFamily:'Times new Roman',fontWeight:'bold'}}>{'\u20B9'}3000/month</span></h3>
+        <h5><span style={{fontFamily:'Times new Roman',fontWeight:'bold'}}>#Full Profile can be viewed</span></h5>
+        <Button onClick={redirectp} color={'dark'}>Pay{'\u20B9'}3000</Button>
+        <h5><span style={{fontFamily:'Times new Roman',fontWeight:'bold'}}>Offer price {'\u20B9'}0 grab the offer</span></h5>
+
+        
+        
+        </CardHeader>
+           </Card>
+           </Col>
+           </Row>
+        
+            </Container>
+
+</BaseMem>
+</div>
+              );
+        }
+        export default Platinum;
+        
